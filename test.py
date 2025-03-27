d0255d40a2ef386ff6941b02169adb92ea0aeca2
WesGale05 <wesjgale05@gmail.com>
2025-03-27 12:38:13 -0500

    psudoCode / began imput for symbol and chart type

---
+++ test.py
@@ -0,0 +1,38 @@
+stock_symbol = None
+chart_type = None
+
+# display title
+print()
+print("Stock Data Visualizer")
+print("---------------------")
+print()
+
+# enter stock symbol
+#while(stock_symbol == None):
+    #prompt for input
+symbol_input = input("Enter Stock Symbol: ")
+    #validate symbol
+        #if symbol found in database:
+            #stock_symbol = symbol_input
+        # if not found
+            #display "Please enter a valid stock symbol"
+            #continue
+
+
+
+# enter chart type
+print("\nChart Types")
+print("-----------")
+print("1. Bar \n2. Line")
+#while(chart_type == None):
+    #prompt for chart type
+chart_input = input("\nEnter the chart type you want (1,2): ")
+    #validate input
+if chart_input == "1":
+    chart_type = 1
+    print("bar graph")
+elif chart_input == "2":
+    charttype = 2
+    print("line graph")
+else:
+    print("Please select a valid option.")
