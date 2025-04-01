c39d1434881d256da17ae66a6c03fbf7fc90ce5c
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-01 16:59:39 -0500

    Removed print statement

--- APIConnector.py
+++ APIConnector.py
@@ -5,7 +5,6 @@ def get_api_connection(type, symbol, interval):
     functionType = type
     stockSymbol = symbol
     stockInterval = interval
-    print(functionType)
     # check if the user requested an intraday type, which requires the use of the interval parameter
     if(functionType == "TIME_SERIES_INTRADAY"):
         url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=RZ44H0Q12VLLXK96" % (functionType, stockSymbol, stockInterval))
