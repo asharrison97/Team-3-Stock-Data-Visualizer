6d4b794041bf6d13985a466b0a764b7a9c10d17b
unknown <asharrison97@gmail.com>
2025-04-04 02:37:40 -0500

    Changed main.py to reflect changes in supporting functions.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 # importing functions from other files
 from APIConnector import get_api_connection
-from Date_Selection import get_date_range
+from start_date import get_start_date
+from end_date import get_end_date
 from chart_type import get_chart_type
 from enter_symbol import get_stock_symbol
 from time_series import get_time_series
+from generate_graph import create_chart
+from intraday_interval import get_intraday_interval
 
 def main():
     stock_symbol = get_stock_symbol()
     chart_type = get_chart_type()
     time_series = get_time_series()
     if(time_series == "TIME_SERIES_INTRADAY"):
-        interval = int(input("\nEnter an appropriate time interval for the INTRADAY function."))
+        interval = get_intraday_interval()
     else:
         interval = None
-    date_range = get_date_range()
+    start_date = get_start_date()
+    end_date = get_end_date(start_date)
     data = get_api_connection(time_series, stock_symbol, interval)
+    print(start_date)
+    print(end_date)
+    create_chart(data, start_date, end_date, chart_type, stock_symbol, interval)
 main()
