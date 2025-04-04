34e56af43d3a2e93812739e2982d587283e06e75
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-03 22:46:52 -0500

    Modified APIConnector.py so that it now returns CSV files instead of JSON files.

--- APIConnector.py
+++ APIConnector.py
@@ -1,4 +1,5 @@
-import requests;
+import requests
+import csv
 
 # this function will accept a function type, stock symbol, and a time interval in order to create a valid API request
 def get_api_connection(time_series, symbol, interval):
@@ -7,12 +8,17 @@ def get_api_connection(time_series, symbol, interval):
     timeInterval = interval
     # check if the user requested an intraday type, which requires the use of the interval parameter
     if(functionTimeSeries == "TIME_SERIES_INTRADAY"):
-        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&datatype=csv&apikey=RZ44H0Q12VLLXK96" % (functionTimeSeries, stockSymbol, timeInterval))
+        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=RZ44H0Q12VLLXK96&datatype=csv" % (functionTimeSeries, stockSymbol, timeInterval))
     else:
-        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&datatype=csv&apikey=RZ44H0Q12VLLXK96" % (functionTimeSeries, stockSymbol))
+        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&apikey=RZ44H0Q12VLLXK96&datatype=csv" % (functionTimeSeries, stockSymbol))
+    print(url)
     # we are using the url constructed to execute a request
     r = requests.get(url)
-    data = r.csv()
+    # use the csv library to read the file
+    data = csv.reader(r)
+    my_list = list(data)
+    for row in my_list:
+        print(row)
     # we are making sure that the status code is 200, which indicates a successful query
     if(r.status_code is 200):
         return data
