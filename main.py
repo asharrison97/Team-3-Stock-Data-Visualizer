7630ca20b33bd794156b0d8ef60102f4aa4e7f6d
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-03 21:42:18 -0500

    Added main.py, which uses all of the function files in the repository to make our program work.

---
+++ main.py
@@ -0,0 +1,16 @@
+# importing functions from other files
+from APIConnector import get_api_connection
+from Date_Selection import get_date_range
+from chart_type import get_chart_type
+from enter_symbol import get_stock_symbol
+from time_series import get_time_series
+
+def main():
+    stock_symbol = get_stock_symbol()
+    chart_type = get_chart_type()
+    time_series = get_time_series()
+    if(time_series == "INTRADAY"):
+        interval = int(input("\nEnter an appropriate time interval for the INTRADAY function."))
+    date_range = get_date_range()
+    get_api_connection(time_series, stock_symbol, interval)
+main()
