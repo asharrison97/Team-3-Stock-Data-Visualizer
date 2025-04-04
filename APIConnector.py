c5ae25f317fda839e252a54aa4c052eede5d6b3f
unknown <asharrison97@gmail.com>
2025-04-04 00:43:03 -0500

    Fixed bug in APIConnector.py, now back to returning .json type files

--- APIConnector.py
+++ APIConnector.py
@@ -8,17 +8,15 @@ def get_api_connection(time_series, symbol, interval):
     timeInterval = interval
     # check if the user requested an intraday type, which requires the use of the interval parameter
     if(functionTimeSeries == "TIME_SERIES_INTRADAY"):
-        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=RZ44H0Q12VLLXK96&datatype=csv" % (functionTimeSeries, stockSymbol, timeInterval))
+        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&interval=%s&apikey=RZ44H0Q12VLLXK96" % (functionTimeSeries, stockSymbol, timeInterval))
     else:
-        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&apikey=RZ44H0Q12VLLXK96&datatype=csv" % (functionTimeSeries, stockSymbol))
+        url = ("https://www.alphavantage.co/query?function=%s&symbol=%s&apikey=RZ44H0Q12VLLXK96" % (functionTimeSeries, stockSymbol))
     print(url)
     # we are using the url constructed to execute a request
     r = requests.get(url)
-    # use the csv library to read the file
-    data = csv.reader(r)
-    my_list = list(data)
-    for row in my_list:
-        print(row)
+    # set data variable equal to the .json file that was returned from the API request
+    data = r.json()
+
     # we are making sure that the status code is 200, which indicates a successful query
     if(r.status_code is 200):
         return data
