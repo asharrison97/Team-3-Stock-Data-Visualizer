152151494eb540843ec68c10fc77163930cc567d
Megan Winans <rlwx3k@umsystem.edu>
2025-04-04 13:39:05 -0500

    Revamped create_chart function, as well as added results function for html file
    Since there were issues with the graph rendering, I tried following the PLA's advice and 3380's stocks example to implement an html file to help with the rendering of the graph. The results.html file will pair with this one.

--- generate_graph.py
+++ generate_graph.py
@@ -1,5 +1,10 @@
 #Function to create a pygal bar or line chart, based on the user's input.
+from flask import Flask, render_template, request, flash, url_for
 import pygal
+
+#create a Flask object
+app = Flask(__name__)
+
 def create_chart(data, start_date, end_date, chart_input, symbol_input, intraday_interval):
     open = []
     high = []
@@ -23,7 +28,7 @@ def create_chart(data, start_date, end_date, chart_input, symbol_input, intraday
         bar_chart.add("Low", low)
         bar_chart.add("Close", close)
     
-        bar_chart.render_in_browser()
+        bar_chart.render_to_file('static/chart.svg')
 
     elif chart_input == "2":
         #Line chart components:
@@ -36,4 +41,31 @@ def create_chart(data, start_date, end_date, chart_input, symbol_input, intraday
         line_chart.add("Low", low)
         line_chart.add("Close", close)
     
-        line_chart.render_in_browser()
+        line_chart.render_to_file('static/chart.svg')
+
+#route/view to display query results submitted from the user's input
+@app.route("/results", methods=['POST'])
+def results():
+    from enter_symbol import get_stock_symbol
+    from APIConnector import get_api_connection
+    from time_series import get_time_series
+    from intraday_interval import get_intraday_interval
+    from start_date import get_start_date
+    from end_date import get_end_date
+    from chart_type import get_chart_type
+
+    symbol = get_stock_symbol()
+    api_connector = get_api_connection(get_time_series(), symbol, get_intraday_interval())
+
+    #assign result variable a default value
+    query_result = None
+
+    #python dictionary for query parameters
+    query = {}
+
+    query["Symbol"] = symbol
+    query_result = list(api_connector.find(query))
+    chart = create_chart(query_result, get_start_date(), get_end_date(), get_chart_type(), get_stock_symbol(), get_intraday_interval())
+
+    #send the results to the results.html template
+    return render_template("results.html", query_result=query_result, chart=chart)
